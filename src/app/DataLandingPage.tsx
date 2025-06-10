0f464355ba07a9db073cfd890d08b38fa5879693
GLD5000 <garethlouisdevlin@gmail.com>
2025-06-10 21:23:34 +0100

    Add List with links

--- src/app/DataLandingPage.tsx
+++ src/app/DataLandingPage.tsx
@@ -1,7 +1,7 @@
 import DataSection from "./DataSection";
 
 export default function DataLandingPage() {
-  return <main className="bg-[#f1f1f1] w-screen h-screen grid">
+  return <main className="bg-[#f1f1f1] w-screen h-full grid">
     <DataSection />
   </main>;
 }
--- src/app/DataSection.tsx
+++ src/app/DataSection.tsx
@@ -1,14 +1,16 @@
 "use client";
 
-import { TypographyH1 } from "./typography";
+import { TypographyH1, TypographyH2, TypographyLink, TypographyList, TypographyListItem } from "./typography";
 import useGlaJson from "./useGlaJson";
 
 export default function DataSection() {
   const glaJson = useGlaJson();
   const { data } = glaJson;
   if (data === null) return null;
-  const { title, description } = data;
-
+  const { title, description, resources } = data;
+  const pdfResources = Object.values(resources).filter(
+    (resource) => resource.format === "pdf"
+  );
   return (
     <div className="grid w-full max-w-[65rem] h-fit mx-auto md:p-4 gap-4 ">
       <div className="flex flex-wrap gap-4 w-fit h-fit my-auto mx-auto">
@@ -19,7 +21,23 @@ export default function DataSection() {
           <div
             dangerouslySetInnerHTML={{ __html: description }}
             className="gla-paragraph text-neutral-700"
-          />
+                  />
+                  <TypographyH2>
+                      Available PDFs
+                  </TypographyH2>
+          <TypographyList>
+            {pdfResources.map((resource, index) => {
+              const { title, url } = resource;
+              return (
+                <TypographyListItem key={`${index}-${title}`}>
+                      <TypographyLink href={`${url}`} target="_blank" className="hover:underline focus:underline">
+                          
+                  {title}
+                </TypographyLink>
+                </TypographyListItem>
+              );
+            })}
+          </TypographyList>
           <style>
             {`.gla-paragraph a {
 color:oklch(42.4% 0.199 265.638)}
