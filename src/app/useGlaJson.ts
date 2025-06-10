f36996fe9d9304a22d03bc0e8c0389f92d66e2d8
GLD5000 <garethlouisdevlin@gmail.com>
2025-06-10 20:39:27 +0100

    Add fetch local

--- src/app/useGlaJson.ts
+++ src/app/useGlaJson.ts
@@ -1,42 +1,64 @@
-import { useState, useEffect, Dispatch, SetStateAction } from 'react';
-import { GlaDataJson } from './glaTypes';
-
+import { useState, useEffect, Dispatch, SetStateAction } from "react";
+import { GlaDataJson } from "./glaTypes";
 
 export default function useGlaJson(): {
-    data: GlaDataJson | null;
-    isLoading: boolean;
-    refetch: () => void;
+  data: GlaDataJson | null;
+  isLoading: boolean;
+  refetch: () => void;
 } {
-    const [data, setData] = useState<GlaDataJson | null>(null);
-    const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState<GlaDataJson | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
 
-    useEffect(() => {
-        fetchJson(setData, setIsLoading);
-    }, [setData, setIsLoading]);
+  useEffect(() => {
+    fetchJsonV2(setData, setIsLoading);
+  }, [setData, setIsLoading]);
 
-    const refetch = (): void => {
-        fetchJson(setData, setIsLoading);
-    };
+  const refetch = (): void => {
+    fetchJsonV2(setData, setIsLoading);
+  };
 
-    return { data, isLoading, refetch };
+  return { data, isLoading, refetch };
 }
 
-async function fetchJson(
-    setData: Dispatch<SetStateAction<GlaDataJson | null>>,
-    setIsLoading: Dispatch<SetStateAction<boolean>>
+export async function fetchJson(
+  setData: Dispatch<SetStateAction<GlaDataJson | null>>,
+  setIsLoading: Dispatch<SetStateAction<boolean>>
 ): Promise<void> {
-    try {
-        const request = {
-            method: "GET",
-            format: "pdf"
-        }
-        const response = await fetch(`https://data.london.gov.uk/api/dataset/gla-adult-skills-fund/`,request);
-        const jsonData = await response.json();
+  try {
+    const request = {
+      method: "GET",
+      format: "pdf",
+    };
+    const response = await fetch(
+      `https://data.london.gov.uk/api/dataset/gla-adult-skills-fund/`,
+      request
+    );
+    const jsonData = await response.json();
 
-        setData(jsonData);
-    } catch (error) {
-        console.error('Error fetching JSON files:', error);
-    } finally {
-        setIsLoading(false);
-    }
+    setData(jsonData);
+  } catch (error) {
+    console.error("Error fetching JSON files:", error);
+  } finally {
+    setIsLoading(false);
+  }
+}
+
+async function fetchJsonV2(
+  setData: Dispatch<SetStateAction<GlaDataJson | null>>,
+  setIsLoading: Dispatch<SetStateAction<boolean>>
+): Promise<void> {
+  try {
+    const request = {
+      method: "GET",
+      format: "pdf",
+    };
+    const response = await fetch(`glaAsf.json`);
+    const jsonData = await response.json();
+
+    setData(jsonData);
+  } catch (error) {
+    console.error("Error fetching JSON files:", error);
+  } finally {
+    setIsLoading(false);
+  }
 }
